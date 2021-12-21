7d80986f46c30f5bd1a9160207f7498490a20cd9
Dale Halford-Pearce <dale@bulb.co.uk>
2021-12-21 14:21:25 +0000

    Day 2 part b

--- solutions/day_02/solution.py
+++ solutions/day_02/solution.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from enum import Enum, auto
 from typing import Generator, TextIO
 
@@ -13,8 +13,8 @@ class Direction(Enum):
 
 @dataclass
 class Point:
-    x: int
-    z: int  # Depth (higher is deeper)
+    x: int = 0
+    z: int = 0  # Depth (higher is deeper)
 
     def __add__(self, o: Point) -> Point:
         return Point(x=self.x + o.x, z=self.z + o.z)
@@ -30,6 +30,26 @@ class Point:
 class Movement:
     direction: Direction
     length: int
+
+
+@dataclass
+class Ship:
+    location: Point = field(default_factory=Point)
+    aim: int = 0
+
+
+    def move_ship(self, move: Movement):
+        match move.direction:
+            case Direction.up:
+                self.aim -= move.length
+            case Direction.down:
+                self.aim += move.length
+            case Direction.forward:
+                v = Point(move.length, move.length * self.aim)
+                self.location += v
+            case _:
+                raise ValueError
+
 
 
 DIRECTION_TO_VECTOR: dict[Direction, Point] = {
@@ -62,5 +82,15 @@ def solve_part_a() -> int:
     return location.x * location.z
 
 
+def solve_part_b() -> int:
+    ship = Ship()
+    with open("solutions/day_02/input.txt") as f:
+        for m in movements_from_file(f):
+            ship.move_ship(m)
+
+    return ship.location.x * ship.location.z
+
+
 if __name__ == "__main__":
     print(solve_part_a())
+    print(solve_part_b())
