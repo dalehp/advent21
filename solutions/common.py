8e65d03c81a70bd662eda76ee978ce9bad2997d1
Dale Halford-Pearce <dale@bulb.co.uk>
2022-01-07 16:38:15 +0000

    Add day 11

--- solutions/common.py
+++ solutions/common.py
@@ -27,14 +27,40 @@ class IntGrid:
                 self.grid[Point(i, j)] = int(character)
 
     def __iter__(self) -> Iterator[tuple[Point, int]]:
-        for position, height in self.grid.items():
-            yield position, height
+        for position, value in self.grid.items():
+            yield position, value
 
     def __getitem__(self, p: Point) -> int:
         return self.grid[p]
 
+    def __setitem__(self, p: Point, v: int):
+        self.grid[p] = v
+
     def __contains__(self, p: Point) -> bool:
         return p in self.grid
+
+    def __len__(self) -> int:
+        return len(self.grid)
+
+    @staticmethod
+    def _char(i: int) -> str:
+        if i == 10:
+            return "*"
+        return str(i)
+
+    def __str__(self) -> str:
+        min_x = min(p.x for p in self.grid.keys())
+        max_x = max(p.x for p in self.grid.keys())
+        min_z = min(p.z for p in self.grid.keys())
+        max_z = max(p.z for p in self.grid.keys())
+
+        return (
+            "\n".join(
+                "".join(self._char(self[Point(i, j)]) for i in range(min_x, max_x + 1))
+                for j in range(min_z, max_z + 1)
+            )
+            + "\n"
+        )
 
     def get(self, p: Point) -> Optional[int]:
         return self.grid.get(p)
---
+++ solutions/day_11/solution.py
@@ -0,0 +1,86 @@
+from typing import TextIO
+
+from solutions.common import IntGrid, Point
+
+FILE = "solutions/day_11/input.txt"
+TEST_FILE = "solutions/day_11/test_input.txt"
+
+DIRECTIONS = (
+    Point(0, 1),
+    Point(1, 1),
+    Point(1, 0),
+    Point(1, -1),
+    Point(0, -1),
+    Point(-1, -1),
+    Point(-1, 0),
+    Point(-1, 1),
+)
+FLASH = 10
+
+
+def increase_energy_levels(grid: IntGrid):
+    for point, energy in grid:
+        grid[point] = energy + 1
+
+
+def process_flash(grid: IntGrid, point: Point):
+    for p in (d + point for d in DIRECTIONS):
+        energy = grid.get(p)
+        if energy is None or energy == FLASH or energy == 0:
+            continue
+        grid[p] = energy + 1
+    grid[point] = 0
+
+
+def process_flashes(grid: IntGrid) -> int:
+    to_flash = []
+    for point, energy in grid:
+        if energy == FLASH:
+            to_flash.append(point)
+    if not to_flash:
+        return 0
+    for p in to_flash:
+        process_flash(grid, p)
+    return len(to_flash)
+
+
+def step(grid: IntGrid) -> int:
+    flashes = 0
+    increase_energy_levels(grid)
+    while flashed := process_flashes(grid):
+        flashes += flashed
+    return flashes
+
+
+def all_flash(grid: IntGrid) -> bool:
+    flashes = step(grid)
+    if flashes == len(grid):
+        return True
+    return False
+
+
+def solve_part_a() -> int:
+    with open(FILE) as f:
+        grid = IntGrid(f)
+
+    flashes = 0
+    for _ in range(100):
+        flashes += step(grid)
+
+    return flashes
+
+
+def solve_part_b() -> int:
+    with open(FILE) as f:
+        grid = IntGrid(f)
+
+    step = 1
+    while not all_flash(grid):
+        step += 1
+
+    return step
+
+
+def run():
+    print(solve_part_a())
+    print(solve_part_b())
